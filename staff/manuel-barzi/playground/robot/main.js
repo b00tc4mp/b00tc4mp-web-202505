bb1562f42b015212588477ad09af9f42c277d198
manuelbarzi <manuelbarzi@gmail.com>
2025-05-15 12:42:46 +0200

    add robot initial version with keyboard events #1

---
+++ staff/manuel-barzi/playground/robot/main.js
@@ -0,0 +1,27 @@
+var robot = document.getElementById('robot')
+
+var step = 10
+var x = 100
+var y = 200
+
+robot.style.left = x + 'px'
+robot.style.top = y + 'px'
+
+document.addEventListener('keydown', function (event) {
+    var key = event.key
+
+    console.log(key)
+
+    if (key === 'ArrowDown') {
+        y = y + step
+    } else if (key === 'ArrowUp') {
+        y = y - step
+    } else if (key === 'ArrowLeft') {
+        x = x - step
+    } else if (key === 'ArrowRight') {
+        x = x + step
+    }
+
+    robot.style.left = x + 'px'
+    robot.style.top = y + 'px'
+})
