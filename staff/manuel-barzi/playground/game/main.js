a2dae98cd3c35c413f3aa436fd0e180df1c5d14f
manuelbarzi <manuelbarzi@gmail.com>
2025-05-16 13:54:01 +0200

    add grass and more water to game #1

--- staff/manuel-barzi/playground/game/main.js
+++ staff/manuel-barzi/playground/game/main.js
@@ -8,6 +8,16 @@ game.style.borderStyle = 'solid'
 game.style.borderWidth = 1 + 'px'
 game.style.position = 'absolute'
 
+var grass = document.createElement('div')
+grass.style.width = 10 + 'px'
+grass.style.height = 10 + 'px'
+grass.style.backgroundColor = 'darkgreen'
+grass.style.position = 'absolute'
+grass.style.left = 20 + 'px'
+grass.style.top = 20 + 'px'
+
+game.appendChild(grass)
+
 var water = document.createElement('div')
 water.style.width = 10 + 'px'
 water.style.height = 10 + 'px'
@@ -17,5 +27,15 @@ water.style.left = 30 + 'px'
 water.style.top = 30 + 'px'
 
 game.appendChild(water)
+
+var water1 = document.createElement('div')
+water1.style.width = 10 + 'px'
+water1.style.height = 10 + 'px'
+water1.style.backgroundColor = 'dodgerblue'
+water1.style.position = 'absolute'
+water1.style.left = 40 + 'px'
+water1.style.top = 40 + 'px'
+
+game.appendChild(water1)
 
 body.appendChild(game)
